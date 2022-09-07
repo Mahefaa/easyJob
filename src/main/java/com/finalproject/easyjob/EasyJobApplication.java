8de9caed4a4fc3ac37256335d8508591487d57c1
Mahefa Ny Anjara <hei.mahefa@gmail.com>
2022-09-07 23:49:53 +0300

    chore: initialize project

---
+++ src/main/java/com/finalproject/easyjob/EasyJobApplication.java
@@ -0,0 +1,13 @@
+package com.finalproject.easyjob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyJobApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EasyJobApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/finalproject/easyjob/EasyJobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finalproject.easyjob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyJobApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
